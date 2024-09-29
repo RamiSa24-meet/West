c5d1f5d36baaba69b0b472617ace99eb3e5f58a2
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2024-09-29 21:32:45 +0300

    wdawd

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gunicorn app:app
+
+# Activate the virtual environment if necessary
+source venv/bin/activate  # Change this path if your venv is located somewhere else
+
+# Run Gunicorn server
+gunicorn app:app  # Replace 'app' with the name of your Flask app file without the .py extension
